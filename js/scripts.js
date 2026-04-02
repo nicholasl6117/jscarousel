2d3519df199c4e4af7a0b5c7436853ca50bcf7c2
nicholasl6117 <nlucci@mail.endicott.edu>
2026-04-02 16:57:28 -0400

    Complete

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,33 @@
 window.onload = function () {
     document.getElementById("copyright").innerHTML = new Date().getFullYear();
 }
+
+// slideshow function
+let intIndex = 0;
+
+function showImage(i) {
+    intIndex += i;
+
+    let theImages = document.getElementsByClassName("image");
+    let theDots = document.getElementsByClassName("dot");
+
+    for(n=0; n < theImages.length; n++) {
+        theImages[n].style.display = "none";
+        theDots[n].className = theDots[n].className.replace(" active", "");
+    }
+
+    if (intIndex > theImages.length-1) {
+        intIndex = 0;
+    }
+
+    if (intIndex < 0) {
+        intIndex = theImages.length-1;
+    }
+
+    theImages[intIndex].style.display = "block";
+    theDots[intIndex].className += " active";
+}
+
+setInterval(() => {
+    showImage(1)
+}, 5000);
